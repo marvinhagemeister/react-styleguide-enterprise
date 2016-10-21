e68c3521985afecfb5dc2606d485254a7e0f3e6a
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-21 20:45:35 +0200

    Cleanup dependencies

--- test/components/Button/Button.js
+++ test/components/Button/Button.js
@@ -1,36 +1,33 @@
 import React, { PropTypes } from 'react';
 
-/**
- * The only true button.
- */
+// The only true button.
 export default function Button({
-	color,
-	size,
-	children,
+  color,
+  size,
+  children,
 }) {
-	let styles = {
-		color,
-		fontSize: Button.sizes[size],
-	};
+  const styles = {
+    color,
+    fontSize: Button.sizes[size],
+  };
 
-	return (
-		<button style={styles}>{children}</button>
-	);
+  return (
+    <button style={styles}>{children}</button>
+  );
 }
 Button.propTypes = {
-	/**
-	 * Button label.
-	 */
-	children: PropTypes.string.isRequired,
-	color: PropTypes.string,
-	size: PropTypes.oneOf(['small', 'normal', 'large']),
+
+	/** Button label */
+  children: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'normal', 'large']),
 };
 Button.defaultProps = {
-	color: '#333',
-	size: 'normal',
+  color: '#333',
+  size: 'normal',
 };
 Button.sizes = {
-	small: '10px',
-	normal: '14px',
-	large: '18px',
+  small: '10px',
+  normal: '14px',
+  large: '18px',
 };
